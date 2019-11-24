5a23607838b80af2b3583193f4bf7f01a1251caa
Jacob Barkdull <jacobbarkdull@gmail.com>
2019-11-24 07:47:12 -0800

    Update Copyright License
    Wrong License

--- src/php-webserver-gui.c
+++ src/php-webserver-gui.c
@@ -3,16 +3,16 @@
  * This file is part of PHP Webserver GUI.
  *
  * PHP Webserver GUI is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License as
+ * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
  * PHP Webserver GUI is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
+ * You should have received a copy of the GNU General Public License
  * along with PHP Webserver GUI.  If not, see <http://www.gnu.org/licenses/>.
  */
 
